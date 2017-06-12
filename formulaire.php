f1c5cf7a611da8123f48dd025f7724f08f00b593
cose_solo <djaafar.boumaza69@gmail.com>
2017-06-12 17:25:29 +0200

    first commit

---
+++ formulaire.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<form action='personsformulaire.php' method='POST'>";
+echo "<label for='nom'>NOM</label>";
+echo "<input type='text' id='nom'>";
+
+echo "<label for=nom>
+echo "</form>";
+
+?>
