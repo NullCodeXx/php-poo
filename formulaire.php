aacb4488d75e0311674b37941a34575a0bc3726b
cose_solo <djaafar.boumaza69@gmail.com>
2017-06-13 11:49:09 +0200

    add class persons and formulaire.

--- formulaire.php
+++ formulaire.php
@@ -1,10 +1,47 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Formulaire</title>
+    </head>
+    <body>
 
-echo "<form action='personsformulaire.php' method='POST'>";
-echo "<label for='nom'>NOM</label>";
-echo "<input type='text' id='nom'>";
+        <?php
+        if (isset($_POST['nom'])) {
 
-echo "<label for=nom>
-echo "</form>";
+            $filtre = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
-?>
+            include_once './Personne.php';
+            $instancePersonne = new Personne($filtre['nom'], $filtre['prenom'], $filtre['age'], $filtre['region']);
+
+            echo '<pre>';
+            var_dump($instancePersonne);
+            echo '</pre>';
+        }
+        ?>
+
+        <form action="personne.php" method="POST">
+
+            <div><label>Nom</label>
+                <input type="text" name="nom" placeholder="Tapez votre nom ici">
+            </div>
+            <div><label>Prénom</label>
+                <input type="text" name="prénom" placeholder="Tapez votre prénom ici">
+            </div>
+            <div><label>Age</label>
+                <input type="number" name="age" >
+            </div>
+            <div><label>region</label>
+                <input type="text" name="region" >
+                <button>Send</button>
+            </div>
+
+
+
+
+        </form>
+
+    </body>
+</html>
---
+++ personne.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of personne
+ *
+ * @author solo
+ */
+class personne {
+    private $nom;
+    private $age;
+    private $region;
+    private $prenom; 
+}
